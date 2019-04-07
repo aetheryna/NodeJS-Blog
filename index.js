a024b1c4c6bd0961ded9deb327e0cef193c20420
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-07 15:42:47 +0530

    mongodb init

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const path = require("path");
 const express = require("express");
 const expressEdge = require("express-edge");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = new express();
 
+mongoose
+  .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
+  .then(() => "You are connected to MongoDB!")
+  .catch(err => console.error("Woops, something went wrong :(", err));
+
 app.use(express.static("public"));
 app.use(expressEdge);
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.set("views", __dirname + "/views");
 
@@ -16,6 +28,11 @@ app.get("/", (req, res) => {
 
 app.get("/post/new", (req, res) => {
   res.render("create");
+});
+
+app.post("/post/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.get("/about", (req, res) => {
