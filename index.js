2685468ebaab2ef6945a1937cec0257c91d8612d
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-08 00:04:52 +0530

    POST method

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/post/new", (req, res) => {
   res.render("create");
 });
 
-app.post("/post/store", (req, res) => {
+app.post("/posts/store", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
