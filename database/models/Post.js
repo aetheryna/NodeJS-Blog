edd294bcdafba67ad159cab7f6d4a49f458f711a
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-12 09:49:34 +0530

    Added a schema

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,7 +4,11 @@ const PostSchema = new mongoose.Schema({
   title: String,
   description: String,
   content: String,
-  username: String
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 const Post = mongoose.model("Post", PostSchema);
